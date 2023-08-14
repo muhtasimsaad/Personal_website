99b7e4036b324239fcba31cfe0c94d917d3af93d
Muhtasim Saad <muhtasimsaad@gmail.com>
2023-08-14 17:11:00 +1000

    update

--- pages/index.js
+++ pages/index.js
@@ -44,8 +44,8 @@ const Index = () => {
 
   return <div className='bg-background'>
 
-    <div className='w-4/6 mx-auto'>
-      <nav className="w-full my-auto text-3xl h-fit">
+    <nav className="fixed w-screen text-3xl bg-transparent bg-red-100 h-fit">
+      <div className="w-2/3 mx-auto bg-background">
         <div className="mx-auto max-w-7xl">
           <div className="relative flex items-center justify-between h-16">
             <div className="flex w-full">
@@ -66,53 +66,59 @@ const Index = () => {
             </div>
           </div>
         </div>
-      </nav>
-      <div className='snap-mandatory snap-y'>
-        <div className='flex h-screen my-auto bg-red-100 snap-center'>
-          <div className='flex my-auto'>
-            <div className='w-1/2'>
-              <p className={`${montserrat_bold.className} text-8xl h-fit my-auto rounded-md py-2`}> Hello<span className='text-secondary'>.</span></p>
-              <p className={`${lobster.className} w-full text-6xl h-fit my-auto rounded-md py-2 text-gray-100`}> This is Saad</p>
-              <p className={`${poppins_normal.className} w-full text-secondary`}>Full Stack Developer</p>
-              <p className='pr-0 text-sm text-gray-100 xl:pr-12'>With expertise in designing and developing web applications, I love creating seamless
-                  user experice and tackling complex challenges.</p>
+      </div>
+    </nav>
+
+
+    <div className='w-full px-2'>
+      <div className='h-screen overflow-auto snap-mandatory snap-y scrollbar-thick scrollbar-thumb-gray-400 scrollbar-track-gray-300"'>
+        <div className='flex h-screen my-auto snap-start'>
+          <div className='w-2/3 m-auto'>
+            <div className='flex my-auto'>
+              <div className='w-1/2'>
+                <p className={`${montserrat_bold.className} text-8xl h-fit my-auto rounded-md py-2`}> Hello<span className='text-secondary'>.</span></p>
+                <p className={`${lobster.className} w-full text-6xl h-fit my-auto rounded-md py-2 text-gray-100`}> This is Saad</p>
+                <p className={`${poppins_normal.className} w-full text-secondary`}>Full Stack Developer</p>
+                <p className='pr-0 text-sm text-gray-100 xl:pr-12'>With expertise in designing and developing web applications, I love creating seamless
+                    user experice and tackling complex challenges.</p>
+
+                <div className='mt-4 w-fit'>
+                  <p className={`${montserrat_bold_600.className} rounded-2xl px-12 py-2 text-background hover:text-secondary bg-secondary 
+                      hover:bg-background text-sm border-2 border-transparent hover:border-secondary cursor-pointer`}>Get In Touch</p>
+                </div>
+              </div>
+              
+              <div className='w-1/2'>
+                <Image src = {home} className="" alt="home"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='flex h-screen my-auto snap-start'>
+          <div className='w-2/3 m-auto'>
+            <div className='flex my-auto'>
+              <div className='flex w-1/2'>
+                <div className='my-auto h-fit'>
+            <p className={`${montserrat_700.className} w-full text-4xl h-fit my-auto rounded-md py-2 text-gray-100`}>
+                MORE<br />ON <span className='text-secondary'>ME</span>.
+              </p>
+              <p className='pr-0 text-sm text-gray-100 xl:pr-12'>Programming isn&apos;t just a job for me, it&apos;s a true passion.
+                I constantly explore new technologies to stay ahead of the curve, whether it&apos;s diving into backend systems 
+                or crafting beautiful front-end interfaces.</p>
+
+              <p className='pr-0 mt-8 text-sm text-gray-100 xl:pr-12'>Beyond coding, I&apos;m also an avid gamer. I find inspiration 
+                in the creativity and immersive experiences that video games offer, which drives me to create engaging and 
+                interactive digital solutions.</p>
 
               <div className='mt-4 w-fit'>
                 <p className={`${montserrat_bold_600.className} rounded-2xl px-12 py-2 text-background hover:text-secondary bg-secondary 
-                    hover:bg-background text-sm border-2 border-transparent hover:border-secondary cursor-pointer`}>Get In Touch</p>
+                    hover:bg-background text-sm border-2 border-transparent hover:border-secondary cursor-pointer`}>See My Work</p>
               </div>
-            </div>
-            
-            <div className='w-1/2'>
-              <Image src = {home} className="" />
-            </div>
-          </div>
-          
-          
-        </div>
-        <div className='flex h-screen my-auto bg-red-200 snap-center'>
-          <div className='flex my-auto'>
-            <div className='flex w-1/2'>
-              <div className='my-auto h-fit'>
-          <p className={`${montserrat_700.className} w-full text-4xl h-fit my-auto rounded-md py-2 text-gray-100`}>
-              MORE<br />ON <span className='text-secondary'>ME</span>.
-            </p>
-            <p className='pr-0 text-sm text-gray-100 xl:pr-12'>Programming isn't just a job for me, it's a true passion.
-              I constantly explore new technologies to stay ahead of the curve, whether it's diving into backend systems 
-              or crafting beautiful front-end interfaces.</p>
-
-            <p className='pr-0 mt-8 text-sm text-gray-100 xl:pr-12'>Beyond coding, I'm also an avid gamer. I find inspiration 
-              in the creativity and immersive experiences that video games offer, which drives me to create engaging and 
-              interactive digital solutions.</p>
-
-            <div className='mt-4 w-fit'>
-              <p className={`${montserrat_bold_600.className} rounded-2xl px-12 py-2 text-background hover:text-secondary bg-secondary 
-                  hover:bg-background text-sm border-2 border-transparent hover:border-secondary cursor-pointer`}>See My Work</p>
-            </div>
+                </div>
               </div>
-            </div>
-            <div className='w-1/2 px-4'>
-              <Image src = {about} className="w-full rounded-lg" />
+              <div className='w-1/2 px-4'>
+                <Image src = {about} alt="about" className="w-full rounded-lg" />
+              </div>
             </div>
           </div>
         </div>
